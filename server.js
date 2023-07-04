c9267d99440b302a1af46c0e72f86893ef4d8f92
Janhavi Alekar <97527096+JanhaviAlekar@users.noreply.github.com>
2023-07-04 23:47:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoute from './routes/productRoute.js'
 import cors from "cors"
 import path from "path"
-import { fileUrlToPath } from 'url';
+import { URL } from 'url';
 //configure env
 dotenv.config();
 
@@ -16,22 +16,20 @@ connnectDB();
 
 //rest object
 const app = express();
-//es-6 module
-const __filename = fileUrlToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 //middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(process.cwd(), 'client', 'build')));
 //routes
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/category', categoryRoutes)
 app.use('/api/v1/product', productRoute)
 //rest api
-app.use('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
+app.use('*', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
